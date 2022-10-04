8f8faf312b3138c517ed27ddd0b75b21252bf70a
Jose Payne <74579909+teck-souport@users.noreply.github.com>
2022-10-04 11:17:53 -0400

    No change

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
 </template>
-
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
