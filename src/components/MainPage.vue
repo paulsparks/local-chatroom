2daed96283d23691d00fe3ab34401a65d3069cd2
paulsparks <psparks1225@gmail.com>
2022-10-07 10:46:38 -0400

    Fixed functions and added main page

---
+++ src/components/MainPage.vue
@@ -0,0 +1,30 @@
+<template>
+    <h1 style="color:white;">test</h1>
+</template>
+
+<script>
+    export default {
+        name: 'MainPage',
+        data() {
+            return {
+
+            }
+        },
+        methods: {
+            removeItem(item) {
+                let element = document.getElementById(item)
+                element.remove()
+            },
+            toggleVisibility(item) {
+                let element = document.getElementById(item)
+                if (element.classList.contains('hiddenElement')) {
+                    element.classList.remove('hiddenElement')
+                    element.classList.add('unhiddenElement')
+                } else {
+                    element.classList.add('hiddenElement')
+                    element.classList.remove('unhiddenElement')
+                }
+            }
+        }
+    }
+</script>
