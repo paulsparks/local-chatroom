1f345c266721cc328e56c66ed962690a0e6a3f84
paulsparks <psparks1225@gmail.com>
2022-10-10 12:47:18 -0400

    Created socket initialization

---
+++ index.js
@@ -0,0 +1,22 @@
+const app = require('express')()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http, {
+    cors: {
+        origins: ['http://localhost:8080']
+    }
+})
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hey socket.io</h1>')
+})
+
+io.on('connection', (socket) => {
+    console.log('a user connected')
+    socket.on('disconnect', () => {
+        console.log('user disconnected')
+    })
+})
+
+http.listen(3000, () => {
+    console.log('listening on *:3000')
+})
