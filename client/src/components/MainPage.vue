07343163ec05e5cb62965d1f1ff0e7d752572245
paulsparks <psparks1225@gmail.com>
2022-10-13 17:23:07 -0400

    Decided against emojis, added image button

--- client/src/components/MainPage.vue
+++ client/src/components/MainPage.vue
@@ -14,11 +14,11 @@
             <div class="col-12" id="chatBox">
                 <div class="container d-flex h-100">
                     <div class="row align-self-center w-100">
-                        <div class="col-1 d-grid">
-                            <i class="bi bi-emoji-smile" style="font-size: 20pt; color: #b8dfff; cursor: pointer;"></i>
+                        <div class="col-1">
+                            <i id="emojiKeyboard" class="bi bi-card-image" style="font-size: 20pt; color: #b8dfff; cursor: pointer;"></i>
                         </div>
                         <div class="col-9">
-                            <input @keyup.enter="sendMessage(message)" v-model="message" class="form-control">
+                            <input id="inputBox" @keyup.enter="sendMessage(message)" v-model="message" class="form-control">
                         </div>
                         <div class="col-2 d-grid">
                             <button class="btn btn-primary" @:click="sendMessage(message)">send</button>
@@ -64,6 +64,7 @@
             socketioService.socket.on('user count', (userCount) => {
                 this.activeUsers = userCount
             })
+
         },
         methods: {
             removeItem(item) {
